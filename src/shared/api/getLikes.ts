76f5ebeb4276183fe044b16f87008a2bcfde4e74
inhoudblotter <inhoudblotter@gmail.com>
2023-10-21 02:22:35 +0500

    fix: fixed loading data with an empty list

--- src/shared/api/getLikes.ts
+++ src/shared/api/getLikes.ts
@@ -2,6 +2,7 @@ import {IContent} from '../types/IContent';
 import {handleError} from './handleError';
 
 export async function getLikes(ids: string[], offset: number = 0, limit: number = 25) {
+  if (ids.length === 0) return {data: [], pagination: {total_count: 0, offset: 0, count: 0}};
   const params = new URLSearchParams({
     api_key: import.meta.env.VITE_API_KEY,
     ids: ids.slice(offset, offset + limit).join(','),
